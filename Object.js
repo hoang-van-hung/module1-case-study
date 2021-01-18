670e9ca8c1a7e082d4ab0714d7cb0255907428f8
unknown <justforyou96hd@gmail.com>
2021-01-18 17:26:46 +0700

    thu 2

---
+++ Object.js
@@ -0,0 +1,20 @@
+class Wallet {
+    constructor() {
+        this.total = 0;
+        this.spent = 0;
+        this.current = 0;
+    }
+    addTotal(number){
+        this.total += number;
+        this.current += number;
+
+    }
+    getCurrentMoney(){
+        return this.current;
+    }
+    spendMoney(amount){
+        this.current -= amount;
+        this.spent +=amount;
+
+    }
+}
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+
 let list =loadData();
 function saveData() {
     localStorage.setItem('item',JSON.stringify(list));
+    localStorage.setItem('amount',JSON.stringify(wallet));
 }
 function loadData() {
     if (localStorage.hasOwnProperty('item')){
@@ -11,71 +13,25 @@ function loadData() {
     }
 }
 
-
-class Wallet {
-    constructor(total) {
-        this.total = total;
-        this.spent = spent;
-        this.rest = rest;
-    }
-    setTotal(){
-        this.total =document.getElementById('total').innerHTML;
-    }
-
-    getRest(){
-
-        return this.rest =this.total -this.spent;
-    }
-    getAlert(){
-        if (this.rest < 0){
-            return alert("You need to save your money!!!");
-        }
+/*function saveData() {
+    localStorage.setItem('amount',JSON.stringify(wallet1));
+}
+function loadData() {
+    if (localStorage.hasOwnProperty('amount')){
+        let data= JSON.parse(localStorage.getItem('amount'));
+        return data;
+    }else {
+        return [];
     }
 }
-
-class List {
-    constructor(date,name,amount,note) {
-        this.date = date;
-        this.name= name;
-        this.amount =amount;
-        this.note =note;
-    }
-    AddItem(){
-        this.date= document.getElementById('list-date').value;
-        this.name= document.getElementById('list-name').value;
-        this.amount= document.getElementById('list-amount').value;
-        this.note= document.getElementById('list-note').value;
-        if (this.date !="" &&this.name !=""&& this.amount !=""){
-            let item =[this.date ,this.name,this.amount];
-            list.push(item);
-            displayList();
-        }else {
-            alert("please enter the infor !!!");
-        }
-    }
-
-    UpdateItem(){
-        this.date =prompt("Input the date : ",list[index],[0]);
-        this.name =prompt("Input the date : ",list[index],[1]);
-        this.amount =prompt("Input the date : ",list[index],[2]);
-        this.note =prompt("Input the date : ",list[index],[3]);
-        let newItem= [this.date,this.name,this.amount,this.note];
-        list[index]= newItem;
-        displayList(newItem);
-    }
-     setDeleteItem(index){
-        list.splice(index,1);
-        this.getDisplayList();
-
-    }
-
-
-}
+let wallet =loadData()*/
+let wallet1= new Wallet();
+wallet1.addTotal(500000);
 
 function displayList(){
     let str ="<tr>" +
-        "<th>Date</th>" +
         "<th>STT</th>" +
+        "<th>Date</th>" +
         "<th>Name</th>" +
         "<th>Amount</th>" +
         "<th>Note</th>" +
@@ -83,20 +39,72 @@ function displayList(){
         "</tr>";
     for (let i = 0; i < list.length; i++) {
        str += `<tr>
-                <td>${list[i][0]}</td>
+                <td>${i+1}</td>
                 <td>${list[i][1]}</td>
                 <td>${list[i][2]}</td>
                 <td>${list[i][3]}</td>
                 <td>${list[i][4]}</td>
-                <td>${list[i][5]}</td>
-                <td><button id="UpdateList" onclick="UpdateList(${i})">Update</button></td>
-                <td><button id="DeleteList" onclick="DeleteList(${i})">Delete</button></td>
-                </tr>`
+                <td><button id="UpdateList" onclick="updateItem(${i})">Update</button></td>
+                <td><button id="DeleteList" onclick="deleteItem(${i})">Delete</button></td>
+                </tr>`;
     }
     document.getElementById('list').innerHTML =str;
+    document.getElementById('total').innerHTML=wallet1.total;
+    document.getElementById('rest').innerHTML=wallet1.current;
+    document.getElementById('spent').innerHTML = parseInt(wallet1.spent);
+
     saveData();
+    // totalWallet();
 }
 displayList();
 
+function addList() {
+    let date= document.getElementById('list-date').value;
+    let name =document.getElementById('list-name').value;
+    let amount=document.getElementById('list-amount').value;
+    let note= document.getElementById('list-note').value;
+
+        if (date !="" &&name !=""&& amount !=""){
+            let item =[ ,date,name,amount,note];
+            list.push(item);
+            addSpend(amount)
+            displayList();
+            resetInput();
+        }else {
+            alert("please enter the infor !!!");
+        }
+    }
+function deleteItem(index) {
+    list.splice(index,1);
+    displayList();
+}
+function updateItem(index){
+    let newDate= prompt("Input new date: ",list[index][1]);
+    let newName= prompt("Input new name: ",list[index][2]);
+    let newAmount= prompt("Input new amount: ",list[index][3]);
+    let newNote= prompt("Input new note: ",list[index][4]);
+    let item=[newDate, , newName,newAmount,newNote];
+    list[index]= item;
+    displayList();
+}
+
+function resetInput() {
+    document.getElementById('list-date').value ="";
+    document.getElementById('list-name').value ="";
+    document.getElementById('list-amount').value ="";
+    document.getElementById('list-note').value ="";
+}
+/*function totalWallet() {
+    let totalSpent=0;
+    let restMoney= 0;
+
+    for (let i = 0; i < list.length; i++) {
+        totalSpent += parseInt(list[i][3]);
+        document.getElementById('rest').innerHTML=restMoney;
+    }*/
+function addSpend(money){
+    parseInt(wallet1.spendMoney(money));
+    saveData();
+}
 
 
