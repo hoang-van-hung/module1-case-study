591be17eb018841f0421dc95bc56111596ce5da9
unknown <justforyou96hd@gmail.com>
2021-01-21 11:52:59 +0700

    fix xong login

--- login-js.js
+++ login-js.js
@@ -1,15 +1,38 @@
-let user= document.getElementById('userName').value;
-let password= document.getElementById('passWord').value;
-function login() {
-    if (user= "admin"){
+
+/*function login() {
+    let username= document.getElementById('username').value;
+    let password= document.getElementById('password').value;
+    if (username == "admin"){
         if (password="123"){
-            <a href="register-form.html">Don't have an account?</a>
+            alert ("Login successfully");
+            window.location = "wallet.html";
 
         }else {
             alert("password wrong!!!");
         }
     }else {
         alert("username wrong!!!");
+    }
+
+}*/
+let attempt =5;
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username == "admin" && password == "123") {
+        alert("Login successfully");
+        window.location = "wallet.html";
+        return false;
+    }else {
+        attempt--;
+        alert("You have left "+attempt+" attempt");
+    }
+    if (acount ==0){
+        document.getElementById('username').disabled=true;
+        document.getElementById('password').disabled=true;
+        document.getElementById('submit').disabled=true;
+        return false;
     }
 
 }
